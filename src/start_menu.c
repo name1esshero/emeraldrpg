278001d3d54bb1fd57b1d587fe34f4151fe60605
name1esshero <mitchellwestbrook45@gmail.com>
2023-01-12 15:32:25 -0600

    remove extra save confirmation

--- src/start_menu.c
+++ src/start_menu.c
@@ -1073,7 +1073,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
