7fefae3995489a189f93a7328907547f802e5066
name1esshero <mitchellwestbrook45@gmail.com>
2023-01-20 22:04:42 -0600

    tweaks

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1702,6 +1702,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
